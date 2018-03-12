e19d74d9c080a6b410c96b7419f59805c1467089
Brehan25885 <brehan.ibrahim@gmail.com>
2018-03-12 14:06:59 +0200

    Last Version:

--- dbms.sh
+++ dbms.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+database_name="databasess"
+if ! [[ -d $database_name ]]
+  then
+    mkdir databasess
+  fi
 cd databasess
 create_menu(){
 read -p "Create Database type 1
@@ -13,7 +17,7 @@ case $choice in
 ;;
 "0") exit 0
 ;;
-*) exit 0
+*) create_menu
 ;;
 esac
 
@@ -142,6 +146,11 @@ else
 }
 drop_table (){
   read -p "Enter table name : " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      tables_menu
+    fi
   rm $table_name
   rm .$table_name
 
@@ -159,8 +168,8 @@ add_record(){
   if ! [[ -f $table_name ]]
     then
       echo "The table doesn't exist choose another table"
-  fi
- num_col=`awk 'END{print NR}' .$table_name`
+else
+   num_col=`awk 'END{print NR}' .$table_name`
   for (( i = 2; i <= $num_col; i++ )); do
     colName=$(awk 'BEGIN{FS="|"}{ if(NR=='$i') print $1}' .$table_name)
     colType=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $2}' .$table_name)
@@ -224,7 +233,9 @@ fi
  fi
 
  row=""
+fi
  tables_menu
+
 
 }
 
@@ -240,15 +251,17 @@ function select_record() {
                 a) read -p "Enter table name : " table_name
                 if [ -f $table_name ]; then
                   cat $table_name
-                  echo "<html>" >> 1.html
+                  echo "<html>" > $table_name.html
 
-                  echo "<Body>" >> 1.html
+                  echo "<Body>" >> $table_name.html
 
-                  awk 'BEGIN{FS="|"; print "<table border="1">"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' $table_name >> 1.html
+                  awk 'BEGIN{FS="|"; print "<table border="1">"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' $table_name >> $table_name.html
 
-                  echo "</Body>" >> 1.html
+                  echo "</Body>" >> $table_name.html
 
-                  echo "</html>" >> 1.html
+                  echo "</html>" >> $table_name.html
+
+                  echo "open the html file ^_^"
 
 
                 else
@@ -257,9 +270,13 @@ function select_record() {
                   ;;
                 b) read -p "Enter table name : " table_name
                   read -p "Enter the col number: " col_no
+                field_no=$(awk -F'|' '{print NF; exit}' $table_name)
+                field=$(( field_no ))
+                if [ $col_no -gt $field ]; then
+                    echo "The field number doesn't exists"
+                    select_record
+                  fi
                 if [ -f $table_name ]; then
-                      field_no=$(awk -F'|' '{print NF; exit}' $table_name)
-                    field=$(( field_no ))
                       if [ $col_no -le $field ]
                       then
                         touch temp
@@ -273,6 +290,7 @@ function select_record() {
                         echo "</Body>" >> $table_name.html
 
                         echo "</html>" >> $table_name.html
+                        echo "open the html file ^_^"
                     else
                       echo "column number not found"
                     fi
@@ -284,9 +302,12 @@ function select_record() {
                 c) read -p "Enter table name : " table_name
                   read -p "Enter the row number: " row_no
                 if [ -f $table_name ]; then
-                      row_no=$(cat date | wc -l)
-                    row=$(( row_no ))
-                      if [ $row_no -le $row ]
+                      row_num=$(cat $table_name | wc -l)
+                    row=$(( row_num ))
+                    if [ $row_no -gt $row ]; then
+                      echo "The row number doesn't exist"
+                      select_record
+                    elif [ $row_no -le $row ]
                       then
                         touch temp
 
@@ -298,6 +319,7 @@ function select_record() {
                         awk 'BEGIN{FS="|"; print "<table border="1">"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' temp >> $table_name.html
                         echo "</Body>" >> $table_name.html
                         echo "</html>" >> $table_name.html
+                        echo "open the html file ^_^"
 
                         else
                       echo "column number not found"
@@ -321,6 +343,11 @@ function select_record() {
 function delete_record() {
   echo -e "Enter Table Name: \c"
   read table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      tables_menu
+    fi
   echo -e "Enter val: \c"
   read val
 del=$(sed -i "/$val/d" $table_name)
@@ -330,9 +357,16 @@ then
 else
   echo "Error deleting Data"
 fi
+tables_menu
+
 }
 function edit_record {
   read -p "Enter table name : " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      tables_menu
+    fi
   read -p "Enter col name : " field
 field_name=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $table_name)
   if [[ $field_name == "" ]]
@@ -352,6 +386,8 @@ field_name=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") 
       echo "The field was updated Successfully"
        fi
     fi
+    tables_menu
+
 }
 
 
@@ -378,12 +414,18 @@ function alter_table() {
     "0")tables_menu
     ;;
     *) echo "wrong choice"
+    tables_menu
     ;;
     esac
   }
   function deleteField() {
 
     read -p "Enter table name : " table_name
+    if ! [[ -f $table_name ]]
+      then
+        echo "The table doesn't exist"
+        alter_table
+      fi
     read -p "Enter the field name: " col_name
     field_no=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$col_name'") print i}}}' $table_name)
     echo "$field_no"
@@ -401,16 +443,25 @@ function alter_table() {
 
       echo "the field was deleted Successfully"
     fi
+    tables_menu
+
   }
   function newField() {
 
     read -p "Enter table name : " table_name
+    if ! [[ -f $table_name ]]
+      then
+        echo "The table doesn't exist"
+        alter_table
+      fi
     read -p "Enter the new col name : " new_col
     read -p "Enter the new col type: " new_type
     case $new_type in
     "int")type="int"
     ;;
     "text")type="text"
+    ;;
+    "date")type="date"
     ;;
     *)echo "wrong choice"
     ;;
@@ -419,10 +470,17 @@ function alter_table() {
     field_no= $(sed -i "1 s/$/|$new_col/" $table_name)
     echo "$new_col|$type" >> .$table_name
     echo "$field_no"
+    echo "The field was added Successfully"
+    tables_menu
   }
 function renameTable() {
 
   read -p "Enter table name : " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      alter_table
+    fi
   read -p "Enter the new name : " new_val
   mv $table_name $new_val
   mv .$table_name .$new_val
@@ -432,6 +490,11 @@ function renameTable() {
 
   function changeType {
     read -p "Enter table name : " table_name
+    if ! [[ -f $table_name ]]
+      then
+        echo "The table doesn't exist"
+        alter_table
+      fi
     read -p "Enter col name : " field
   field_no=$(awk "/$field/{ print NR; exit }" .$table_name)
   echo "$field_no"
@@ -464,10 +527,17 @@ function renameTable() {
         echo "The field was updated Successfully"
          fi
       fi
+      tables_menu
+
   }
 
   function changeField(){
   read -p "Enter table name : " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      alter_table
+    fi
   read -p "Enter col name : " field
 field_name=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") print i}}}' $table_name)
   if [[ $field_name == "" ]]
@@ -482,26 +552,40 @@ field_name=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field'") 
     echo "The field was updated Successfully"
 
     fi
+    tables_menu
+
 }
 
 display_table(){
   read -p "Enter table name: " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      tables_menu
+    fi
  cat .$table_name
+ tables_menu
 
 }
 sort_table(){
   read -p "Enter table name : " table_name
+  if ! [[ -f $table_name ]]
+    then
+      echo "The table doesn't exist"
+      tables_menu
+    fi
   read -p "Enter the field name: " col_name
   field_no=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$col_name'") print i}}}' $table_name)
-echo "$field_no"
   if [[ $field_no == "" ]]
   then
     echo "Not Found"
     tables_menu
 else
 fieldN=$(( field_no ))
-  echo "$fieldN"
-  cat $table_name | sort -V -k "$fieldN" $table_name
+    head -n1 $table_name
+    sed 1d $table_name | sort -V -k "$fieldN"
 fi
+tables_menu
+
 }
 create_menu
